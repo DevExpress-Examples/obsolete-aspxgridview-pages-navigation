d415b5e3c07c0105cfd418307fd21ffca015d2ca
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CS/WebSite/Default.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+
+    protected void ASPxGridView1_DataBound (object sender, EventArgs e) {
+        ASPxGridView grid = (ASPxGridView)sender;
+        SaveJsProperties(grid);
+    }
+    protected void ASPxGridView1_CustomCallback (object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        ASPxGridView grid = (ASPxGridView)sender;
+        switch (e.Parameters) {
+            case "Next":
+                grid.PageIndex += 1;
+                break;
+            case "Prev":
+                grid.PageIndex -= 1;
+                break;
+        }
+        SaveJsProperties(grid);
+    }
+    private void SaveJsProperties (ASPxGridView grid) {
+        grid.JSProperties["cpPageCount"] = grid.PageCount;
+        grid.JSProperties["cpPageIndex"] = grid.PageIndex;
+    }
+}
