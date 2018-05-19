1f8c5d6b40fc37726670e766b7f621f976bfa191
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# OBSOLETE: ASPxGridView - How to use the keyboard for pages navigation 
+
+
+<p><strong>This example is now obsolete. Use the approach illustrated in the </strong><a href="https://www.devexpress.com/Support/Center/p/E2003">E2003: Using keyboard for ASPxGridView pages navigation</a><strong> example instead.</strong></p><p>This example illustrates how to use the keyboard to switch the ASPxGridView page according to the arrow key that has been pressed. Please refer to the E2237 example, where this solution is described (<a href="https://www.devexpress.com/Support/Center/p/E2237">Using keyboard for ASPxDataView pages navigation</a>).</p>
+
+<br/>
+
+
---
+++ VB/CS/WebSite/Default.aspx.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+
+	Protected Sub ASPxGridView1_DataBound(ByVal sender As Object, ByVal e As EventArgs)
+		Dim grid As ASPxGridView = CType(sender, ASPxGridView)
+		SaveJsProperties(grid)
+	End Sub
+	Protected Sub ASPxGridView1_CustomCallback(ByVal sender As Object, ByVal e As ASPxGridViewCustomCallbackEventArgs)
+		Dim grid As ASPxGridView = CType(sender, ASPxGridView)
+		Select Case e.Parameters
+			Case "Next"
+				grid.PageIndex += 1
+			Case "Prev"
+				grid.PageIndex -= 1
+		End Select
+		SaveJsProperties(grid)
+	End Sub
+	Private Sub SaveJsProperties(ByVal grid As ASPxGridView)
+		grid.JSProperties("cpPageCount") = grid.PageCount
+		grid.JSProperties("cpPageIndex") = grid.PageIndex
+	End Sub
+End Class
